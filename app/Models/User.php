0c76bac960b9f25058d35bbcf4bc4591d20bb4c6
Enysssss <e.tchenar@gmail.com>
2025-09-22 16:24:33 +0200

    Ajout Role et Permissions grace à spatie

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, HasRoles, Notifiable;
 
     protected $table = 'users';
 
@@ -50,22 +51,11 @@ class User extends Authenticatable
         ];
     }
 
-    // app/Models/User.php
-
-    // public function dish()
-    // {
-    //     return $this->hasOne(Dish::class);
-    // }
-
     public function dishes()
     {
         return $this->belongsToMany(Dish::class);
     }
 
-    public function didi()
-    {
-        return $this->hasMany(Dish::class, 'user_id');
-    }
 
     public function likedDishes()
     {
---
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class RolesAndPermissionsSeeder extends Seeder
+{
+    public function run(): void
+    {
+        Permission::firstOrCreate(['name' => 'Create Dish']);
+        Permission::firstOrCreate(['name' => 'Edit Dish']);
+
+        $admin = Role::firstOrCreate(['name' => 'admin']);  
+        $admin->givePermissionTo('Create Dish');
+        $admin->givePermissionTo('Edit Dish');
+
+        $user = User::find(1);
+        if ($user) {
+            $user->assignRole('admin');
+        }
+    }
+}
